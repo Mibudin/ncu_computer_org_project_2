ba65c541caaec6c57988d7f415d6f5e00797054e
Mibudin <jj9028j9028@gmail.com>
2021-06-29 04:09:06 +0800

    Quicksort ends?

--- commands/multiply.sh
+++ commands/multiply.sh
@@ -6,7 +6,7 @@ source /home/mibudin/Workspaces/ncu_computer_org_project_2/commands/config.sh
 
 
 # Write-back cache
-.${COMMANDS_PATH}multiply-write-back.sh
+${COMMANDS_PATH}multiply-write-back.sh
 
 # Write-through cache
-.${COMMANDS_PATH}multiply-write-through.sh
+${COMMANDS_PATH}multiply-write-through.sh
--- commands/quicksort-small.sh
+++ commands/quicksort-small.sh
@@ -6,7 +6,7 @@ source /home/mibudin/Workspaces/ncu_computer_org_project_2/commands/config.sh
 
 
 # 2-way associative cache
-.${COMMANDS_PATH}quicksort-small-2-way.sh
+${COMMANDS_PATH}quicksort-small-2-way.sh
 
 # Full-way associative cache
-.${COMMANDS_PATH}quicksort-small-full-way.sh
+${COMMANDS_PATH}quicksort-small-full-way.sh
